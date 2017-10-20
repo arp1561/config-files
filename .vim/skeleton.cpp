86058c8370d78063d96b121025fc28b53639b469
arp1561 <joshiarpit2@gmail.com>
2017-10-20 15:01:04 +0530

    For 17.10 update

---
+++ .vim/skeleton.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+#define int long long
+#define input(a,n) for(int i=0;i<n;i++) cin>>a[i]
+#define loop(i,a,b) for(int i=a;i<b;i++)
+using namespace std;
+signed main()
+{
+
+}
